0722a9597c85d2ac81d890ec37563a1278aa7ae9
D C <dc@mbp-de-d.gateway.2wire.net>
2014-03-12 23:26:30 -0600

    first

--- ExampleTable/ViewController.m
+++ ExampleTable/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSMutableArray *arreglo;
+}
 
 @end
 
@@ -18,12 +20,51 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    arreglo=[[NSMutableArray alloc]initWithObjects:@"Hola mundo",@"Hello World", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark TableViewDataSource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    
+    //Regresa el numero de secciones en la tabla
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    //regresa el numero de celdas en nuestra tabla
+    return [arreglo count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text=[arreglo objectAtIndex:indexPath.row];
+    
+    return cell;
+    
+}
+
+
+#pragma mark UITableViewDelegate
+
+-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
 }
 
 @end
